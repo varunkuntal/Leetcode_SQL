0e19e444a634cd0623092badf070543bc54c7497
Varun Kuntal <varunkuntal@yahoo.in>
2022-05-30 18:00:35 +0530

    Day 8/10 of Leetcode SQL study plan - Day 71 of #100DaysOfCode

---
+++ Easy/total_time_spent_by_each_employee.sql
@@ -0,0 +1,5 @@
+# Problem URL: https://leetcode.com/problems/find-total-time-spent-by-each-employee
+
+select event_day  as day, emp_id, sum(out_time - in_time) as total_time
+from Employees
+group by event_day, emp_id;
